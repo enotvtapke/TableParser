4795865a3b1b26caec50699ed305a718d4a777d4
Aleksandr Stupnikov <super-stupnikow@mail.ru>
2022-08-24 01:50:07 +0300

    Fixes

--- src/main/kotlin/domain/EntityBuilder.kt
+++ src/main/kotlin/domain/EntityBuilder.kt
@@ -1,20 +1,22 @@
 package domain
 
-import domain.EntityBuilder.asEntityName
-import domain.EntityBuilder.getUnqualifiedName
 import domain.parser.DbService
 import domain.parser.EntityBuildException
-import domain.parser.MigrationBuildException
-import domain.tables.BspbIncomeViews
 import domain.tables.vwRptGetClients.VwRptGetClients
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.javatime.JavaLocalDateColumnType
 import org.jetbrains.exposed.sql.javatime.JavaLocalDateTimeColumnType
 import org.jetbrains.exposed.sql.javatime.JavaLocalTimeColumnType
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.math.BigDecimal
-import java.time.LocalDate
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import java.nio.file.StandardOpenOption.CREATE
+import java.nio.file.StandardOpenOption.TRUNCATE_EXISTING
 import java.util.*
+import kotlin.io.path.createDirectories
+import kotlin.io.path.createFile
 
 fun main() {
     DbService.setupDb()
@@ -24,7 +26,15 @@ fun main() {
 }
 
 object EntityBuilder {
+    private var indent: Int = 0
     private val imports: MutableList<Import> = mutableListOf()
+
+    private enum class Import(val type: String) {
+        BigDecimal("java.math.BigDecimal"),
+        LocalTime("java.time.LocalDate"),
+        LocalDate("java.time.LocalDate"),
+        LocalDateTime("java.time.LocalDateTime")
+    }
 
     private fun String.getSchema(): String {
         val schemaRegex = Regex("^([^.]+)\\.")
@@ -52,20 +62,21 @@ object EntityBuilder {
         }
     }
 
+    private fun String.toSingular() =
+        if (this.endsWith("es")) {
+            this.removeSuffix("es")
+        } else if (this.endsWith("s")) {
+            this.removeSuffix("s")
+        } else {
+            this
+        }
+
     private fun getClassName(tableName: String): String {
         val className = tableName.getUnqualifiedName().asEntityName()
             .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-        if (className.endsWith("es")) {
-            return className.removeSuffix("es")
-        } else if (className.endsWith("s")) {
-            return className.removeSuffix("s")
-        } else {
-            return className
-        }
+        return className.toSingular()
 
     }
-
-    private var indent: Int = 0
 
     private fun StringBuilder.add(content: String) {
         append("\t".repeat(indent) + content)
@@ -93,8 +104,8 @@ object EntityBuilder {
             block()
         }
 
-    private fun IColumnType.asKotlinType() =
-        when (this) {
+    private fun IColumnType.asKotlinType(): String {
+        val type = when (this) {
             is LongColumnType, is EntityIDColumnType<*> -> "Long"
             is DecimalColumnType -> {
                 imports.add(Import.BigDecimal)
@@ -118,29 +129,46 @@ object EntityBuilder {
             else -> throw EntityBuildException("Unknown column type ${this.javaClass}");
         }
 
+        return type + if (this.nullable) "?" else ""
+    }
+
+    private const val tablesPackagePrefix = "domain.tables."
+    private const val entitiesBasePackage = "domain.entities."
+
+    private val entitiesBaseFolder = Path.of("C:\\Users\\super\\work\\TableParser\\src\\main\\kotlin\\domain\\entities")
+
+    private fun getPackage(canonicalName: String) = canonicalName.dropLastWhile { it != '.' }.dropLast(1)
+
+    private fun getEntityPackage(table: Table) =
+        entitiesBasePackage + getPackage(table.javaClass.canonicalName).removePrefix(tablesPackagePrefix).toSingular()
+
+    private fun entityPackageToPath(entityPackage: String): Path =
+        Path.of(entityPackage.removePrefix(entitiesBasePackage).replace(".", File.separator))
+
     fun build(table: Table): String {
         // TODO add package
-        val packageCode = ""
+        val packageCode = "package ${getEntityPackage(table)}\n"
+        val className = getClassName(table.tableName)
         val entityCode = buildString {
-            dataClass(getClassName(table.tableName)) {
+            dataClass(className) {
                 table.columns.forEach {
                     column(it)
                 }
             }
         }
+
         val importsCode = buildString {
-            imports.forEach {
-                append("import ${it.type}")
+            imports.distinct().forEach {
+                append("import ${it.type}\n")
             }
         }
-        return packageCode + importsCode + entityCode
-    }
 
+        val code = packageCode + "\n" + importsCode + "\n" + entityCode
 
-    private enum class Import(val type: String) {
-        BigDecimal("java.math.$this"),
-        LocalDate("java.time.$this"),
-        LocalTime("java.time.$this"),
-        LocalDateTime("java.time.$this")
+        val pathToEntity = entitiesBaseFolder.resolve(entityPackageToPath(getEntityPackage((table))))
+        pathToEntity.parent.createDirectories()
+        // todo What???
+        Files.write(pathToEntity.resolve("$className.kt"), code.toByteArray())
+        return code
     }
 }
